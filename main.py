a980579b4afefbe0b49df64b82c32762da01f44c
Aydan Thompson <aydanthompson20@gmail.com>
2023-10-13 16:29:36 +0100

    Various formatting updates

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
 import math
-import random
+import sys
+
 import pygame
 import pygame.font
 import pygame.time
@@ -33,35 +33,41 @@ class Body:
         self.update_radius()
 
     def update_radius(self):
-        self.radius = math.sqrt(self.mass) * 2
+        # Assume the mass is the area of the circle.
+        self.radius = math.sqrt(self.mass / math.pi)
         return self
 
-    def update_force(self, other_body: "Body", bodies: list):
-        gravitational_constant = 0  # 6.67430e-11
+    def update_force(self, bodies: list["Body"]):
+        """Updates the force applied to this body for each body in `bodies`."""
+        for second_body in bodies:
+            if self != second_body:
+                self._update_force(second_body)
+        return self
 
-        dx = self.pos_x - other_body.pos_x
-        dy = self.pos_y - other_body.pos_y
-        distance = math.sqrt(dx**2 + dy**2)
+    def _update_force(self, second_body: "Body"):
+        # In our universe, G (gravitational constant) is equal to 6.67430e-11, but in
+        # this simulation G can be set to different values to change the proportionality
+        # of the force applied to each body.
+        G = 1.0
 
-        if distance <= self.radius + other_body.radius:
+        dx = self.pos_x - second_body.pos_x
+        dy = self.pos_y - second_body.pos_y
+        d = math.sqrt(dx**2 + dy**2)
+
+        if d <= self.radius + second_body.radius:
             force = 0
             # self.combine(other_body, bodies)
         else:
-            force = -(gravitational_constant * self.mass * other_body.mass) / (
-                distance**2
-            )
+            # Newton's Universal Law of Gravitation:
+            # https://phys.libretexts.org/Bookshelves/Conceptual_Physics/Introduction_to_Physics_(Park)/02%3A_Mechanics_I_-_Motion_and_Forces/02%3A_Dynamics/2.09%3A_Newtons_Universal_Law_of_Gravitation
+            force = -(G * self.mass * second_body.mass) / (d**2)
 
         angle = math.atan2(dy, dx)
         self.force_x += force * math.cos(angle)
         self.force_y += force * math.sin(angle)
         return self
 
-    def reset_force(self):
-        self.force_x = 0.0
-        self.force_y = 0.0
-        return self
-
-    def update_position(self, timestep: float = 1):
+    def update_position(self, timestep: float = 1.0):
         # a = F / m
         self.accel_x = self.force_x / self.mass
         self.accel_y = self.force_y / self.mass
@@ -75,6 +81,13 @@ class Body:
         # s2 = s1 + vt
         self.pos_x += self.vel_x * timestep
         self.pos_y += self.vel_y * timestep
+
+        self._reset_force()
+        return self
+
+    def _reset_force(self):
+        self.force_x = 0.0
+        self.force_y = 0.0
         return self
 
     def combine(self, other_body: "Body", bodies: list):
@@ -90,9 +103,16 @@ class Body:
         bodies.remove(other_body)
 
     def update_trail(self):
+        """Adds the current position to the trail and removes the oldest position from
+        the trail if the number of points in the trail is too high.
+        """
+        trail_max = 2**8
+
         self.trail.append((self.pos_x, self.pos_y))
 
-        if len(self.trail) > 2**6:
+        # Remove the oldest position from the trail if
+        # the number of points exceeds `trail_max`.
+        if len(self.trail) > trail_max:
             self.trail.pop(0)
         return self
 
@@ -121,11 +141,10 @@ def main() -> None:
 
     # Add the various bodies.
     bodies: list["Body"] = []
-
     bodies.append(
         Body(
             "a",
-            mass=10,
+            mass=250,
             pos_x=(WINDOW_SIZE[0] / 2) - 100,
             pos_y=WINDOW_SIZE[1] / 2,
             color=(255, 0, 0),
@@ -135,7 +154,7 @@ def main() -> None:
     bodies.append(
         Body(
             "b",
-            mass=10,
+            mass=250,
             pos_x=(WINDOW_SIZE[0] / 2) + 100,
             pos_y=WINDOW_SIZE[1] / 2,
             color=(0, 0, 255),
@@ -156,17 +175,15 @@ def main() -> None:
         window.fill((0, 0, 0))
 
         # Update the force applied to each body.
-        for primary_body in bodies:
-            for secondary_body in bodies:
-                if primary_body != secondary_body:
-                    primary_body.update_force(secondary_body, bodies)
+        for body in bodies:
+            body.update_force(bodies)
 
         for body in bodies:
             # Update the position and trail.
             body.update_position(timestep=timestep)
             body.update_trail()
 
-            # Redraw the body and trail.
+            # Draw the body and trail.
             pygame.draw.circle(
                 window,
                 body.color,
@@ -180,7 +197,6 @@ def main() -> None:
                     False,
                     body.trail,
                 )
-            body.reset_force()
 
         # Draw the framerate, frametime, and the number of
         # bodies in the top-left corner of the window.
